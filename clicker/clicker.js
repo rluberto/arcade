ce51c1bc56393967768b3f44388a63376cc2412c
Ray Luberto <rluberto15@gmail.com>
2021-09-16 18:26:46 -0400

    lower volume

--- clicker/clicker.js
+++ clicker/clicker.js
@@ -18,8 +18,8 @@ var gameIsOver = 0;
 //Initialize the game
 function init(){
     musicBox();
-    audioElement0.volume = 0.0001;
-    audioElement1.volume = 0.0001;
+    audioElement0.volume = 0.00000001;
+    audioElement1.volume = 0.00000001;
     document.getElementById("musicTogg").style.display = "inline-block";
     canvas.style.display = "block"; //Get rid of the welcome screen and get the game canvas ready to fade in
     canvas.style.opacity = 0;
